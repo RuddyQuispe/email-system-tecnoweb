e0b1b2d644edd2b6186f1cc4048c4ff72dc72caa
zuleny <zuleny.cr@gmail.com>
2021-12-17 12:51:41 -0400

    pago crud: data and bussiness

---
+++ src/main/java/edu/bo/uagrm/ficct/inf513/business/PagoBusiness.java
@@ -0,0 +1,144 @@
+package edu.bo.uagrm.ficct.inf513.business;
+
+import edu.bo.uagrm.ficct.inf513.data.PagoData;
+
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PagoBusiness {
+    private PagoData pagoData;
+
+    public PagoBusiness() {
+        this.pagoData = new PagoData();
+    }
+
+    /**
+     * create a new Pago
+     * @param parameters list of parameters
+     * @return a message
+     */
+    public String createPago(List<String> parameters) {
+        if (parameters.size() != 8) return "data Pago incomplete";
+        try {
+            ResultSet response = this.pagoData.findBy("nro_pago", parameters.get(0));
+            if (response == null) return "Error to search Pago";
+            if (!response.next()) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                dateFormat.setLenient(false);
+                Date fechaPago = (Date) dateFormat.parse(parameters.get(1).trim());
+                boolean isCreatedPago = this.pagoData.create(Integer.parseInt(parameters.get(0)),fechaPago, Double.parseDouble(parameters.get(2)), parameters.get(3), Double.parseDouble(parameters.get(4)), Integer.parseInt(parameters.get(5)), Integer.parseInt(parameters.get(6)), Integer.parseInt(parameters.get(7)));
+                return isCreatedPago ? "saved Pago successfully" : "I have an error to create Pago";
+            } else {
+                return "Does exists Pago , don't have repeat";
+            }
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace();
+            return "I have an error to create Pago";
+        }
+    }
+
+    /**
+     * get all data
+     * @param parameters
+     * @return a list of data(the first list have the attributes names)
+     */
+    public ArrayList<ArrayList<String>> findAll(){
+        ResultSet data = this.pagoData.findAll();
+        return this.getDataList(data);
+    }
+
+    /**
+     * get all data by a specific attribute
+     * @param parameters
+     * @return a list of data(the first list have the attributes names)
+     */
+    public ArrayList<ArrayList<String>> findBy(List<String> parameters){
+        ResultSet data = this.pagoData.findBy(parameters.get(0), parameters.get(1));
+        return this.getDataList(data);
+    }
+
+    /**
+     * update a pago
+     * @param parameters list of parameters
+     * @return a message
+     */
+    public String updatePago(List<String> parameters) {
+        if (parameters.size() != 8) return "data Pago incomplete";
+        try {
+            ResultSet response = this.pagoData.findBy("nro_pago", parameters.get(0));
+            if (response == null) return "Error to search nro_pago Pago";
+            if (response.next()) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                dateFormat.setLenient(false);
+                Date fechaPago = (Date) dateFormat.parse(parameters.get(1).trim());
+                boolean isUpdatedPago = this.pagoData.update(Integer.parseInt(parameters.get(0)), fechaPago, Double.parseDouble(parameters.get(2)), parameters.get(3), Double.parseDouble(parameters.get(4)), Integer.parseInt(parameters.get(5)), Integer.parseInt(parameters.get(6)), Integer.parseInt(parameters.get(7)));
+                return isUpdatedPago ? "updated Pago successfully" : "I have an error to update Pago";
+            } else {
+                return "Doesn't exists Pago ";
+            }
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace();
+            return "I have an error to update Pago";
+        }
+    }
+
+    /**
+     * delete a specific pago
+     * @param parameters list of parameters
+     * @return a message
+     */
+    public String removePago(List<String> parameters) {
+        if (parameters.size() != 1) return "data Pago incomplete";
+        try {
+            ResultSet response = this.pagoData.findBy("nro_pago", parameters.get(0));
+            if (response == null) return "Error to search nro_pago Pago";
+            if (response.next()) {
+                boolean isRemovedPago = this.pagoData.remove(Integer.parseInt(parameters.get(0)));
+                return isRemovedPago ? "removed Pago successfully" : "I have an error to remove Pago";
+            } else {
+                return "Doesn't exists Pago ";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "I have an error to remove Pago";
+        }
+    }
+
+    /**
+     * Get all rows of a table
+     */
+    private ArrayList<ArrayList<String>> getDataList(ResultSet rs) {
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        try {
+            ResultSetMetaData metadata = rs.getMetaData();
+            int quantityColumns = metadata.getColumnCount();
+            int j = 0;
+            result.add(new ArrayList<String>());
+            for (int i = 1; i <= quantityColumns; i++) {
+                result.get(0).add(metadata.getColumnName(i));
+            }
+            while (rs.next()) {
+                j++;
+                result.add(new ArrayList<String>());
+                for (int i = 1; i <= quantityColumns; i++) {
+                    result.get(j).add(rs.getString(i));
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        PagoBusiness Pago = new PagoBusiness();
+        ArrayList<ArrayList<String>> response = Pago.findAll();
+        System.out.println(response);
+    }
+}
---
+++ src/main/java/edu/bo/uagrm/ficct/inf513/data/PagoData.java
@@ -0,0 +1,148 @@
+package edu.bo.uagrm.ficct.inf513.data;
+import edu.bo.uagrm.ficct.inf513.data.conection_database.ConnectionDB;
+
+import java.sql.*;
+
+public class PagoData {
+    private final ConnectionDB connection;
+
+    /**
+     * @Constructor
+     */
+    public PagoData() {
+        this.connection = ConnectionDB.getInstance();
+    }
+
+    /**
+     * create a new Pago
+     * @param fechaPago date of Pago
+     * @param monto the quantity to pay
+     * @param comprobante
+     * @param montoMora the quantity to pay for late
+     * @param ciSocio ci of the socio
+     * @param ciSecretaria ci of the secretaria
+     * @param idAporte id of aporte
+     * @return true if the pago was created successfully else return, false have an error
+     */
+    public boolean create(int nroPago, Date fechaPago, Double monto, String comprobante, Double montoMora, int ciSocio, int ciSecretaria, int idAporte) {
+        try {
+            // string query structure
+            String query = "insert into pago(nro_pago, fecha_pago, monto, comprobante, monto_mora, ci_socio, ci_secretaria, id_aporte)"
+                    + " values(?,?,?,?,?,?,?)";
+            // get object connection to add Pago information to make
+            PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement(query);
+            preparedStatement.setInt(1, nroPago);
+            preparedStatement.setDate(2, fechaPago);
+            preparedStatement.setDouble(3, monto);
+            preparedStatement.setString(4, comprobante);
+            preparedStatement.setDouble(5, montoMora);
+            preparedStatement.setInt(6, ciSocio);
+            preparedStatement.setInt(7, ciSecretaria);
+            preparedStatement.setInt(8, idAporte);
+            // execute query with its data
+            if (preparedStatement.executeUpdate() == 0) {
+                System.err.println("error in: Class PagoData > create()");
+                throw new SQLException();
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * get all Pago data
+     * @return result query sql
+     */
+    public ResultSet findAll() {
+        try {
+            String query = "select * from pago;";
+            Statement statement = this.connection.getConnection().createStatement();
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * find by attribute one data selected
+     * @param attribute the attribute to find by
+     * @param data the attribute value
+     * @return result query sql
+     */
+    public ResultSet findBy(String attribute, String data) {
+        try {
+            String query = "select * from pago p where p." + attribute + " ='" + data + "';";
+            Statement statement = this.connection.getConnection().createStatement();
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * update Pago data
+     * @param nroPago
+     * @param fechaPago
+     * @param monto
+     * @param comprobante
+     * @param montoMora
+     * @param ciSocio
+     * @param ciSecretaria
+     * @return true if updated success else return, false have an error
+     */
+    public boolean update(int nroPago, Date fechaPago, Double monto, String comprobante, Double montoMora, int ciSocio, int ciSecretaria, int idAporte) {
+        try {
+            // string query structure
+            String query = "update pago set " +
+                    "fecha_pago=?, monto=?, comprobante=?,  monto_mora=?, ci_socio=?, ci_secretaria=?, id_aporte=? " +
+                    "where nro_pago=?;";
+            // get object connection to add Pago information to make
+            PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement(query);
+            preparedStatement.setDate(1, fechaPago);
+            preparedStatement.setDouble(2, monto);
+            preparedStatement.setString(3, comprobante);
+            preparedStatement.setDouble(4, montoMora);
+            preparedStatement.setInt(5, ciSocio);
+            preparedStatement.setInt(6, ciSecretaria);
+            preparedStatement.setInt(7, idAporte);
+            preparedStatement.setInt(8, nroPago);
+            // execute query with its data
+            if (preparedStatement.executeUpdate() == 0) {
+                System.err.println("error in: Class PagoData > update()");
+                throw new SQLException();
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * delete Pago data
+     * @param nroPago :nroPago Pago
+     * @return true if removed success else return false
+     */
+    public boolean remove(int nroPago) {
+        try {
+            String query = "delete from pago where nro_pago=?;";
+            PreparedStatement preparedStatement = this.connection.getConnection().prepareStatement(query);
+            preparedStatement.setInt(1, nroPago);
+            if (preparedStatement.executeUpdate() == 0) {
+                System.err.println("error in: Class PagoData > remove()");
+                throw new SQLException();
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
